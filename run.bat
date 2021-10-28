0abb7cdf9cbf106d23d2480965b3c5498bcffd00
talentedev <webdev124@outlook.com>
2021-10-28 14:36:00 +0100

    stripe integration

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ set /p msg="Enter commit msg: "
 set /p commitDate="Enter commit date: "
 
 ::change repository path
-cd F:\repo
+cd E:\gatsby
 git add .
 set GIT_COMMITTER_DATE="%commitDate%"
 git commit -m "%msg%"
