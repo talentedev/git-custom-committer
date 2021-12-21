dd050113355e61a83273db8afa4ac599cfa08c2c
talentedev <webdev124@outlook.com>
2021-12-22 02:02:39 +0900

    add readme

--- run.bat
+++ run.bat
@@ -3,7 +3,8 @@
 set /p msg="Enter commit msg: "
 set /p commitDate="Enter commit date: "
 
-::cd %path%
+::change repository path
+cd F:\github\repo
 git add .
 set GIT_COMMITTER_DATE="%commitDate%"
 git commit -m "%msg%"
